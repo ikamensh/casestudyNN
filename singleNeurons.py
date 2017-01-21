8c016e9c5feeb82a5799eaa349762ed06a63a01a
ikamensh <ikamenshchikov@gmail.com>
2017-01-21 21:51:27 +0100

    Update singleNeurons.py

--- singleNeurons.py
+++ singleNeurons.py
@@ -1,3 +1,55 @@
+"""
+Backpropagation:
+
+Virtually any optimizer use backpropagation to adjust weights of the layers of neural network. The name Backpropagation comes as opposite to forward pass - calculating the output of the network given its input. 
+The adjustment made to each weight is initially defined as partial derivative of the output w.r.t. the weight. Such partial derivatives are found using chain rule for the weights of hidden layers.
+
+e.g. assume the network has a single input i=1; 
+a hidden layer of size 3 with initial weights W1 = -1, 1, 1
+and an output layer with weights W2(T) = 0.5, -2, 2.
+
+Assume we optimize the layer to achieve output of 1e12; let us follow the evolution of weights under backpropagation with learning rate = 1.
+(singleNeurons.py)
+
+Epoch #0
+
+1: [array([[-1.,  1.,  1.]]]
+
+2: [array([	[ 0.5],
+       		[-2. ],
+       		[ 2. ]]]
+Epoch #1
+1: [array([[-0.5, -1. ,  3. ]]]
+
+2: [array([	[-0.5],
+       		[-1. ],
+       		[ 3. ]]]
+Epoch #2
+1: [array([[-1., -2.,  6.]], )]
+
+2: [array([	[-1.],
+       		[-2.],
+       		[ 6.]], )]
+Epoch #3
+
+1: [array([[ -2.,  -4.,  12.]], )]
+
+2: [array([	[ -2.],
+       		[ -4.],
+       		[ 12.]], )]
+
+in this case the analytical formula for the output is: O = SUMMj (W1j*W2j) where j=1..3, which means that 
+dO / dW1j = W2j 
+dO / dW2j = W1j
+
+This is indeed verified by the output of the test script.
+
+"""
+
+
+
+
+
 from keras.optimizers import SGD, Adam, Adadelta
 from keras.models import Model, load_model # basic class for specifying and training a neural network
 import numpy as np
